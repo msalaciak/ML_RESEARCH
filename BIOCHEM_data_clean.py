ba69275fbd08a676807dbaeb506677e2853c8f32
matthew <matt.salaciak@gmail.com>
2020-01-19 23:01:17 -0500

    merging biochem dataset with progression / follow up datasets

--- BIOCHEM_data_clean.py
+++ BIOCHEM_data_clean.py
@@ -6,24 +6,61 @@ pd.set_option('display.max_columns', None)
 
 #Load dataframes from excel file into pandas
 
-df = pd.read_excel('clean_Data/DLBCL_BIOCHEM/DLBCL_BIOCHEM_v2.xlsx')
-print(df.head(10))
+biochem = pd.read_excel('clean_Data/DLBCL_BIOCHEM/DLBCL_BIOCHEM_v2.xlsx')
+progression_info = pd.read_excel('clean_Data/clean merged/primary-join-follow-up.xlsx')
 
 #Corrects date to proper format
-df['OREDERED_DATE']= pd.to_datetime(df['OREDERED_DATE'],infer_datetime_format=True)
+biochem['OREDERED_DATE']= pd.to_datetime(biochem['OREDERED_DATE'], infer_datetime_format=True)
 
 #rename column
-df.rename(columns={'OREDERED_DATE':'Test Date'}, inplace=True)
+biochem.rename(columns={'OREDERED_DATE': 'Test Date'}, inplace=True)
+biochem.rename(columns={'RES_ID': 'ID'}, inplace=True)
 
 
 #make each test_id an individual column
-df = df.pivot(columns='TEST_ID', values='RESULT')
 
-print(df.head(10))
+biochem = biochem.pivot_table('RESULT', ['ID', 'ORDER_ID', 'CLINIC_ID', 'DOCTOR_ID', 'ORDERING_WORKSTATION_ID', 'Test Date'], 'TEST_ID', aggfunc='first')
+biochem = biochem.reset_index()
 
-#save to new excel.
-writer = pd.ExcelWriter('clean_Data/DLBCL_BIOCHEM/DLBCL_BIOCHEM_v2_test.xlsx', engine='xlsxwriter')
 
-df.to_excel(writer, sheet_name='Sheet1')
+# print(df.head(10))
 
-writer.save()
+# # # creating a list of blank entries , indexNames2 is organized by ORDER column so we can make exclude file, df.drop deletes from this list
+indexNames = biochem[(biochem['CREI'] == '.') & (biochem['GLUI'] == '.') & (biochem['GLUII'] == '.')].index
+indexNames2 = biochem[(biochem['CREI'] == '.') & (biochem['GLUI'] == '.') & (biochem['GLUII'] == '.')].ORDER_ID
+biochem.drop(indexNames, inplace=True)
+
+#Convert GLUI,CREI,GLUII to floats
+biochem["CREI"] = pd.to_numeric(biochem["CREI"], errors='coerce')
+biochem["GLUI"] = pd.to_numeric(biochem["GLUI"], errors='coerce')
+biochem["GLUII"] = pd.to_numeric(biochem["GLUII"], errors='coerce')
+print(biochem.dtypes)
+
+#create a new column that consists of the larger of the two values from GLUI and GLUII
+biochem["GLUCOSE"] = np.nanmax(biochem[["GLUI", "GLUII"]].values, axis=1)
+
+
+
+print(biochem.head(10))
+
+progression_info = progression_info[['ID', 'Progression post RCHOP (yes = 1, no=0)', 'Date Prog after RCHOP for DLBCL', 'DATE_DLBCL Diagnosis']]
+
+merge = pd.merge(biochem, progression_info, on='ID', how='inner')
+
+print(merge.head(10))
+
+exclude = pd.merge(merge, progression_info, on = 'ID', how = 'outer', indicator=True)
+
+exclude = exclude.query('_merge != "both"')
+
+print("EXCLUDED")
+print(exclude.head(10))
+
+
+# save to new excel.
+# writer = pd.ExcelWriter('clean_Data/DLBCL_BIOCHEM/DLBCL_BIOCHEM_GLUCOSE_v2_clean_3.xlsx', engine='xlsxwriter')
+#
+# biochem.to_excel(writer, sheet_name='Sheet1')
+#
+# writer.save()
+
